754ba1ece76c2adf830cb32e6c0574cfecafffa1
Samuel Tabe <61163104+samueltabe@users.noreply.github.com>
2023-10-05 21:31:00 +0100

    newest

--- database/migrations/2023_07_28_222215_add_status_id_to_houses_table.php
+++ database/migrations/2023_07_28_222215_add_status_id_to_houses_table.php
@@ -15,7 +15,7 @@ class AddStatusIdToHousesTable extends Migration
     {
         Schema::table('houses', function (Blueprint $table) {
             $table->unsignedBigInteger('status_id')->after('user_id')->nullable();
-            $table->foreign('status_id')->references('id')->on('status');
+            $table->foreign('status_id')->references('id')->on('statuses');
         });
     }
 
