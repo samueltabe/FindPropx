54fc46afb74459b11ee7e2f68d4b566ec721f362
Samuel Tabe <61163104+samueltabe@users.noreply.github.com>
2024-02-20 13:52:41 +0100

    new update

--- resources/views/inc/frontend/navsection.blade.php
+++ resources/views/inc/frontend/navsection.blade.php
@@ -6,40 +6,39 @@
         </a>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-            <li class="nav-item dropdown active">
-                <a class="nav-link" href="{{url('/')}}" aria-haspopup="true" aria-expanded="false">Home</a>
-            </li>
-            <li class="dropdown nav-item">
-                <a href="{{url('/about')}}" class="nav-link">About Us</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link " href="{{ url('/list') }}" aria-haspopup="true" aria-expanded="false">
-                All Properties
-                </a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link " href="{{ url('/service') }}" aria-haspopup="true" aria-expanded="false">
-                Services
-                </a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link " href="{{ url('/contact') }}" aria-haspopup="true" aria-expanded="false">
-                Contact Us
-                </a>
-            </li>
+                <li class="nav-item dropdown active">
+                    <a class="nav-link" href="{{url('/')}}" aria-haspopup="true" aria-expanded="false">Home</a>
+                </li>
+                <li class="dropdown nav-item">
+                    <a href="{{url('/about')}}" class="nav-link">About Us</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link " href="{{ url('/list') }}" aria-haspopup="true" aria-expanded="false">
+                        All Properties
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link " href="{{ url('/service') }}" aria-haspopup="true" aria-expanded="false">
+                        Services
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link " href="{{ url('/contact') }}" aria-haspopup="true" aria-expanded="false">
+                        Contact Us
+                    </a>
+                </li>
             </ul>
         </div>
         <div class="d-flex align-items-center">
             <div class="call me-4">
                 {{-- <a href="tel:1-800-555-1234"><i class="fa fa-phone me-2 fa-flip-horizontal"></i>1-800-555-1234 </a> --}}
             </div>
-            {{-- <div class="login me-4">
-                <a data-bs-toggle="modal" data-bs-target="#loginModal" href="#">Hello sign in<i class="fa fa-user ps-2"></i></a>
-            </div> --}}
-            <div class="add-listing d-none d-sm-block">
+            <div class="login me-4">
+                {{-- <a data-bs-toggle="modal" data-bs-target="#loginModal" href="#">Hello sign in<i class="fa fa-user ps-2"></i></a> --}}
+            </div>
+            <div class="add-listing">
                 <a data-bs-toggle="modal" data-bs-target="#loginModal" class="btn btn-primary btn-md" href="#">Join in !<i class="fa fa-user ps-2"></i></a>
             </div>
         </div>
     </div>
 </nav>
-
